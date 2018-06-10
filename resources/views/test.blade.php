aa3bef29500d952031ff9eba4bb9bc37436bff84
Andrii Semianovych <saintdronchik@gmail.com>
2018-06-10 20:43:52 +0300

    initial project version

---
+++ resources/views/test.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Тег FORM</title>
+</head>
+
+<body>
+
+	<form action="/comments" method="post">
+		//@csrf
+		<p><b>Как по вашему мнению расшифровывается аббревиатура &quot;ОС&quot;?</b></p>
+		<p><input type="radio" name="answer" value="a1">Офицерский состав
+			<Br>
+			<input type="radio" name="answer" value="a2">Операционная система
+
+			<input type="radio" name="answer" value="a3">Большой полосатый мух</p>
+		<p><input type="submit"></p>
+	</form>
+
+</body>
+
+</html>
